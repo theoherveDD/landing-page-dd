1ab71065ae5a1749c01fd1415d96719d445a11c3
theoherv <theo.dancingdead@gmail.com>
2025-03-17 18:09:18 +0100

    Commir

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/landing-page-dd/', // Assurez-vous que ce chemin est correct pour ton projet
   plugins: [react()],
 });
